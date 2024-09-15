e944f9c4390008b4902a4419e959761627539088
Ampli-xD <dishantkapoor29@gmail.com>
2024-09-15 13:55:17 +0530

    commiteddd

--- PineconeAPIHandler.py
+++ PineconeAPIHandler.py
@@ -1,7 +1,6 @@
 class VectorDBProcessor:
-    def __init__(self, api_key: str, index_name: str):
+    def __init__(self, api_key: str):
         self.api_key = api_key
-        self.index_name = index_name
     
-    def verifier(self):
+    def verifier(self, api_key):
         return True
